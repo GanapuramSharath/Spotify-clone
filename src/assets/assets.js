5d582a558ad0c3354bc9328b4f0048b1f7acbd03
Sharath Teja <sharathteja19@gmail.com>
2025-07-26 15:31:32 +0530

    Initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,230 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpeg'
+import img6 from './img6.jpeg'
+import img7 from './img7.jpeg'
+import img8 from './img8.jpeg'
+import img9 from './img9.png'
+import img10 from './img10.jpeg'
+import img11 from './img11.jpeg'
+import img12 from './img12.jpeg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpeg'
+import img16 from './img16.jpeg'
+import img17 from './img17.jpeg'
+import img18 from './img18.jpeg'
+import img19 from './img19.jpeg'
+import img20 from './img20.jpeg'
+import img21 from './img21.jpeg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+import song9 from  './song9.mp3'
+import song10 from  './song10.mp3'
+import song11 from  './song11.mp3'
+import song12 from  './songs12.mp3'
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Old songs",
+        image: img8,
+        desc:"🎶 Timeless Telugu classics from older decades — nostalgic hits.",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Liked Songs ",
+        image: img9,
+        desc:"💜 A personalized collection of your most played or saved tracks.",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Eye Fantancy Songs👀",
+        image: img10,
+        desc:"👁️ Romantic and dreamy songs — mostly Telugu love melodies.",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "DSP Hits Songs",
+        image: img16,
+        desc:"🔥 A collection of mass and melody hits from Devi Sri Prasad (DSP), a popular Tollywood music director.",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "A.R Rahman Songs",
+        image: img11,
+        desc:"Soulful melodies and iconic tracks by A.R. Rahman",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Geetanjali Songs",
+        image: img17,
+        desc:"💑 Songs from the classic Telugu movie “Geetanjali” — known for its emotional and romantic melodies.",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "🌌💫jamurathiri jabilamma",
+        image: img1,
+        file:song1,
+        desc:"🎶 A classic romantic duet featuring Venkatesh and Sridevi, known for its vintage charm and melodious tune.",
+        duration:"3:00"
+    },
+    {
+        id:1,
+        name: "🌙 Vennelave Vennelave",
+        image: img2,
+        file:song3,
+        desc:"🎼 A romantic classic composed by A.R. Rahman, known for its soothing melody and heartfelt vocals under the moonlit mood.",
+        duration:"3:34"
+    },
+    {
+        id:2,
+        name: "👁️💫nee choopule naa oopiri",
+        image: img3,
+        file:song2,
+        desc:"💞 Romantic duet from Endukante Premanta, featuring Ram and Tamannaah.",
+        duration:"3:47"
+    },
+    {
+        id:3,
+        name: "💞 Niluvadhamu Ninu Epudaina",
+        image: img4,
+        file:song4,
+        desc:"🎶 A soulful love track composed by Devi Sri Prasad (DSP), known for its emotional depth and melodious flow.",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "💔Na Cheli Rojave",
+        image: img5,
+        file:song5,
+        desc:"An emotional love ballad from Roja, capturing the pain of separation.",
+        duration:"2:26"
+    },
+    {
+        id:5,
+        name: "🌅 Naaloney Pongaynu",
+        image: img15    ,
+        file:song6,
+        desc:"💖 A heart-touching romantic track with soothing melody and deep emotional expression.",
+        duration:"2:45"
+    },
+    {
+        id:6,
+        name: "💓My Heart is Beating",
+        image: img7,
+        file:song7,
+        desc:"💃 A catchy, upbeat track composed by Devi Sri Prasad (DSP) — blending English lyrics with Telugu style and energy.",
+        duration:"1:22"
+    },
+    {
+        id:7,
+        name: "❤️nuvvunte naa jathaga ",
+        image: img12,
+        file:song8,
+        desc:"🌹 A soulful love ballad from I, capturing deep emotions with beautiful lyrics and A.R. Rahman’s magical touch.",
+        duration:"2:35"
+    } ,
+    {
+        id:8,
+        name: "😢 Baahubali Sad BGM",
+        image: img18,
+        file:song9,
+        desc:"🗡️🌧️ An emotional orchestral piece echoing sacrifice and legacy",
+        duration:"2:35"
+    },
+    {
+        id:9,
+        name: "🕉️Bharatha Vedamuga",
+        image: img19,
+        file:song10,
+        desc:"🔱🕯️ A divine and powerful devotional song invoking Lord Shiva, rich in spiritual depth",
+        duration:"4:10"
+    },
+    {
+        id:10,
+        name: "💞 Mellaga",
+        image: img20,
+        file:song11,
+        desc:"☔🌧️ A gentle romantic melody with a rain-kissed vibe, soft and soothing",
+        duration:"3:21"
+    }
+    ,
+    {
+        id:11,
+        name: "🤖 Robo (Enthiran)",
+        image: img21,
+        file:song12,
+        desc:"⚡🤖 A high-energy futuristic score blending science and emotion",
+        duration:"1:38"
+    }
+]
